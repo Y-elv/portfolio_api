fb3c2f3ab83be58b1ea03bd954df22072f5e4d13
mugisha <mugishaelvis456@gmail.com>
2023-08-15 17:30:06 +0200

    adding dotenv to hide my password

--- controllers/sendmessage.js
+++ controllers/sendmessage.js
@@ -1,4 +1,33 @@
-const SendMessage=(req,res)=>{
-res.send("now i can send a message")
+import nodemailer from 'nodemailer'
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const SendMessage=async(req,res)=>{
+    try {
+        const { email, message,name } = req.body;
+    
+        const transporter = nodemailer.createTransport({
+          service: 'Gmail', 
+          auth: {
+            user: 'mugishaelvis456@gmail.com',
+            pass: `${process.env.PASSWORD}`
+          }
+        });
+    
+        const mailOptions = {
+          from: email,
+          to: 'mugishaelvis456@gmail.com',
+          subject: 'Portfolio Contact Form Submission',
+          text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+        };
+    
+        await transporter.sendMail(mailOptions);
+        res.status(200).send('Email sent successfully!');
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while sending the email.');
+      }
+    
 }
 export default SendMessage
