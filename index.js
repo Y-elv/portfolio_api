de954ecf69f5558c370fa865e9baa52848a63773
mugisha <mugishaelvis456@gmail.com>
2023-08-30 19:31:59 +0200

    adding some changes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import Welcome from "./controllers/welcome.js";
 import sendMessage from "./routes/sendMessage.js"
 import swaggerDocs from "./api-docs/swagger.js";
+
+
 const app = express();
 app.use(express.json());
 app.use(cors({origin:"*"}));
@@ -10,6 +12,7 @@ app.use(cors({origin:"*"}));
 app.get("/", Welcome)
 
 app.use("/api/v1", sendMessage)
+
 const port = 5200;
 app.listen(port, () => {
     console.log("your server has been started " + port);
